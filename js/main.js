f01b878c41cad5b4170a361c8ca3226c54f0a202
Nathan Reidy <nathanjreidy@gmail.com>
2020-04-24 21:55:28 +1000

    Added event listeners

--- js/main.js
+++ js/main.js
@@ -18,7 +18,57 @@ let vNine = document.querySelector("#nine");
 
 let vScreen = document.querySelector("#textScreenNum");
 
+// Create variables to store current total and current number 
+let currentTotal = 0;
+let currentNum = 0;
+
+// Create event listeners
+
 vClear.addEventListener('click', () => vScreen.textContent = "0");
+
+vZero.addEventListener('click', function () {
+    vScreen.textContent = "0";
+    currentNum = 0; 
+})
+
+// This complicates things significantly, because the screen text needs to change 
+// vPoint.addEventListener('click', function () {
+//     vScreen.textContent = "0";
+//     currentNum = 0; 
+// })
+
+//Need to create calculate function or display final calculation result (currentTotal?)
+// vEquals.addEventListener('click', function () {
+//     calculate(); 
+// })
+
+vOne.addEventListener('click', function () {
+    vScreen.textContent = "1";
+    currentNum = 1; 
+})
+
+vTwo.addEventListener('click', function () {
+    vScreen.textContent = "2";
+    currentNum = 2; 
+})
+
+vThree.addEventListener('click', function () {
+    vScreen.textContent = "3";
+    currentNum = 3; 
+})
+
+//Fix this
+// vSubtract.addEventListener('click', function () {
+//     subtract(); 
+// })
+
+
+vZero.addEventListener('click', function () {
+    vScreen.textContent = "0";
+    currentNum = 0; 
+})
+
+
 
 
 
